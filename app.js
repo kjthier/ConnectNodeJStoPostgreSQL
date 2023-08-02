62a8960668b10adb784efa78adae6432d09bf367
kjthier <karinjthier@gmail.com>
2023-08-02 21:43:05 +0200

    step 5 complete

--- app.js
+++ app.js
@@ -1,41 +1,97 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const port = 3000;
+const pg = require('pg');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// middleware to handle parsing the HTTP request body (from api client, ie insomnia))
+app.use(express.json())
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// setup db connection to elephantSQL
+var conString = "postgres://gwwggekp:FthhupUhL8VRRBDypM05lF_tMuV4cBB5@snuffleupagus.db.elephantsql.com/gwwggekp"
+var client = new pg.Client(conString);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  } else {
+    console.log('Connected to the database');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    // get all the users
+    app.get('/', (req, res) => {
+      client.query('SELECT * FROM users', (error, result) => {
+        if (error) {
+          console.error('Error fetching users:', error);
+          res.status(500).send('Error fetching users');
+        } else {
+          res.status(200).send(result.rows);
+        }
+    })
+    });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+    // get one user (with the id)
+    app.get('/:id', (req, res) => {
+      client.query('SELECT * FROM users WHERE id = $1', [req.params.id], (error, result) => {
+        if (error) {
+          console.error('Error fetching user:', error);
+          res.status(500).send('Error fetching user');
+        } else {
+          res.status(200).send(result.rows);
+        }
+      })
+    })
+
+    // create a new user
+    app.post('/', (req, res) => {
+      client.query('INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3)', [req.body.first_name, req.body.last_name, req.body.age], (error, result) => {
+        if (error) {
+          console.error('Error creating user:', error);
+          res.status(500).send('Error creating user');
+        } else {
+          res.status(201).send('User added successfully!');
+        }
+      })
+    })
+
+    // edit one user
+    app.put('/:id', (req, res) => {
+      client.query('UPDATE users SET first_name = $1, last_name = $2, age = $3 WHERE id = $4', [req.body.first_name, req.body.last_name, req.body.age, req.params.id], (error, result) => {
+        if (error) {
+          console.error('Error updating user:', error);
+          res.status(500).send('Error updating user');
+        } else {
+          res.status(200).send('User updated successfully!');
+        }
+      })
+    })
+
+    // delete one user
+    app.delete('/:id', (req, res) => {
+      client.query('DELETE FROM users WHERE id = $1', [req.params.id], (error, result) => {
+        if (error) {
+          console.error('Error deleting user:', error);
+          res.status(500).send('Error deleting user');
+        } else {
+          res.status(200).send('User deleted successfully!');
+        }
+      })
+    })
+//practice making requests
+//cont with #6
+
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+// start the server
+app.listen(port, (error) => {
+  if(!error) {
+    console.log(`Server is running on port ${port}`);
+  } else {
+    console.log(error);
+  }
 });
 
-module.exports = app;
+
+
